098316700835c9faa28cd7312e87acafc0647532
info <info@sciits.com>
2016-09-09 10:49:42 +0800

    Added gulp into global installer

--- ig.bat
+++ ig.bat
@@ -1,1 +1,1 @@
-npm install -g cordova ionic && npm install http-server -g && npm i bower -g && cd tests && npm i node-debug -g && npm i protractor -g && webdriver-manager update --standalone
+npm install -g cordova ionic && npm install http-server -g && npm install -g gulp && npm i bower -g && cd tests && npm i node-debug -g && npm i protractor -g && webdriver-manager update --standalone
